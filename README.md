16dd7d74bb66d3f7423283b92b742814cb47cafd
Andrey Salnikov <a.salnikov.home@gmail.com>
2018-03-31 00:36:57 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# LMSnew
+Learning Management System: Project for studying Spring, Hibernate frameworks and stages of software development
---
+++ src/main/java/ua/pp/jjd/lmsnew/LMSnewApplication.java
@@ -0,0 +1,12 @@
+package ua.pp.jjd.lmsnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LMSnewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LMSnewApplication.class, args);
+    }
+}
